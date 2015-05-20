8ee94bdb5383cf990eaa1be433c3834f7e5e9dbe
Felix Plitzko <felix.plitzko@libermentix.com>
2015-05-20 20:45:26 +0200

    add deploy script.

--- shell.sh
+++ shell.sh
@@ -4,5 +4,4 @@ rm -rf dist/
 g rm --cached -r dist && g c -m "remove dist" 
 BROCCOLI_TACO_ENV=production broccoli-taco build dist
 g a dist && g c -m "add dist again"
-g subtree push --prefix dist origin master-tmp
-g checkout master && g rebase master origin/master-tmp
+g subtree push --prefix=dist/ git@github.com:eutm/eutm.github.io.git master
