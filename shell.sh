1b54ba7b8f66ef8f06e88cd22ee5acb07459e79d
Felix Plitzko <felix.plitzko@libermentix.com>
2015-06-12 14:43:48 +0200

    removing from master branch.

--- shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf dist/
-g rm --cached -r dist && g c -m "remove dist" 
-BROCCOLI_TACO_ENV=production broccoli-taco build dist
-g a dist && g c -m "add dist again"
-g subtree push --prefix=dist/ git@github.com:eutm/eutm.github.io.git master
--- site/data.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	title: 'Euregio Tech Meetup',
-	name: 'Euregio Tech Community by EuTM',
-	fb_publisher: 'https://www.facebook.com/EuregioTechMeetup',
-	fb_author: 'https://www.facebook.com/EuregioTechMeetup',
-}
